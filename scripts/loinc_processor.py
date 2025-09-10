62c41e10713a180e96ec5ded0770f1b0363904aa
xiaohongchen01 <xiaohongchen01@gmail.com>
2025-09-10 12:41:17 -0400

    npi csv completed

--- scripts/loinc_processor.py
+++ scripts/loinc_processor.py
@@ -1,3 +1,30 @@
 import pandas as pd
 
-lionc = pd.read_csv('input/Loinc.csv',nrows=100)
+## Input/loinc.csv to load up the LOINC codes
+loinc = pd.read_csv('input/Loinc.csv')
+
+###loinc.info() to search for the columns
+loinc.info()
+
+### checking the potential column names we want to keep: loinc_NUM, definitiondescription, LONG_COMMON_NAME to find which is the most relevant data
+loinc.LOINC_NUM.describe()
+loinc.DefinitionDescription.describe()
+loinc.LONG_COMMON_NAME.describe()
+
+### create smaller dataframe with only the columns we want to keep
+loinc_small = loinc[['LOINC_NUM', 'LONG_COMMON_NAME']]
+
+### add column to state last updated date
+loinc_small["last_updated"] = '2025-09-07'
+
+loinc_small = loinc_small.rename(columns={
+    'LOINC_NUM' : 'Code',
+    'LONG_COMMON_NAME' : 'Description',
+    'last_updated' : 'Last Updated'
+})
+
+### determine location of where I want the csv file to be at
+file_output_path = "output/csv/loinc_small.csv"
+
+### export to csv with no false
+loinc_small.to_csv(file_output_path, index=False)
---
+++ scripts/npi_processor.py
@@ -0,0 +1,30 @@
+#import polars and pandas
+import polars as pl
+import pandas as pd
+
+#define where npi file is located
+npi_file_path = 'input/npidata_pfile_20050523-20250810.csv'
+
+#read npi files with polars because it is a larger database for pandas with small rows to see what the columns are
+df_polars = pl.read_csv(npi_file_path, n_rows=10)
+
+#use print to see the columns
+print(df_polars.columns) 
+
+#selecting columns for smaller dataframe
+df_polars_small = df_polars.select(['NPI', 'Provider Last Name (Legal Name)'])
+
+#adding column to small dataframe
+df_polars_small = df_polars_small.with_columns(
+    pl.lit('2025-09-10').alias('Last_Updated')
+)
+
+
+#rename columns to match other dataframes
+df_polars_small = df_polars_small.rename({
+    'NPI' : 'NPI code',
+    'Provider Last Name (Legal Name)' : "Provider Last Name",
+    'Last_Updated' : 'Last_Updated'
+})
+
+
