090bbffe72838f128215808e1e76c286aaa31de1
Xiao Hong Chen <xiaohongchen01@gmail.com>
2025-09-11 04:16:49 -0400

    FINISHED

---
+++ scripts/rxnorm_processor.py
@@ -0,0 +1,54 @@
+#import polars and pathlib
+import polars as pl
+from pathlib import Path
+
+#direct file path
+file_path = Path('input/RXNATOMARCHIVE.RRF')
+
+#put in column names based on the file structure
+columns = [
+    'rxaui', 'aui', 'str', 'archive_timestamp', 'created_timestamp', 
+    'updated_timestamp', 'code', 'is_brand', 'lat', 'last_released', 
+    'saui', 'vsab', 'rxcui', 'sab', 'tty', 'merged_to_rxcui'
+]
+
+#have polars read the file into a dataframe
+df = pl.read_csv(
+    file_path,
+    separator='|',
+    has_header=False,
+    new_columns=columns,
+    truncate_ragged_lines=True
+)
+
+#view the dataframe
+print(df)
+print(df.columns)
+
+
+#very long columns so i want to see all the columns
+pl.Config.set_tbl_cols(-1)  #shows all columns with no limits
+pl.Config.set_tbl_rows(5) #to show only the first 5 rows
+pl.Config.set_tbl_formatting("ASCII_FULL_CONDENSED") #readability
+print(df.head(5))
+
+
+#i like the columns "code" and "str"
+df = df[['code', 'str']]
+
+#add last updated columnn
+df = df.with_columns(pl.lit('2025-08-27').alias('Last_Updated'))
+
+#rename and relist columns
+df + df.rename({
+    'code' : 'RxNorm_Code',
+    'str' : 'Description',
+    'Last_Updated' : 'Last_Updated'
+})
+
+#put output path and save as csv
+output_dir = Path('output/csv/') #specify output path
+output_dir.mkdir(exist_ok=True) #makes sure output directoy exists
+output_path = output_dir / 'RXNATOMACHIVE.csv' #specify full output file path
+
+df.write_csv(output_path)
---
+++ scripts/snowmed_processor.py
@@ -0,0 +1,57 @@
+# import polars and pathlib
+import polars as pl
+from pathlib import Path
+
+#get direct file path
+file_path = Path('input/sct2_Description_Full-en_US1000124_20250301.txt')
+
+#specify the column names based on the file structure
+df = pl.read_csv(
+    file_path,
+    separator='\t',
+    has_header=True,
+    quote_char=None,
+    encoding='utf8-lossy',
+    truncate_ragged_lines=True,
+    dtypes={
+        'id': pl.Utf8,
+        'effectiveTime': pl.Utf8,
+        'active': pl.Int32,
+        'moduleId': pl.Utf8,
+        'conceptId': pl.Utf8,
+        'languageCode': pl.Utf8,
+        'typeId': pl.Utf8,
+        'term': pl.Utf8,
+        'caseSignificanceId': pl.Utf8
+    }
+)
+
+#view the dataframe
+print(df.head(5))
+print(df.columns)
+
+#columns are too long and im not sure what is the best choice for description
+pl.Config.set_tbl_cols(-1) #show all columns with no limitation
+print(df.head(5))
+
+
+#i like the columns "ConceptId" and "term"
+df = df[['conceptId', 'term']]
+
+#add last_updated column
+df = df.with_columns(pl.lit("2025-02-11").alias('Last_Updated'))
+
+#rename and relist columns
+df = df.rename({
+    'conceptId' : 'SNOWMED_Code' ,
+    'term' : 'Description',
+    'Last_Updated' : 'Last_Updated'
+})
+
+#put output path
+output_dir = Path('output/csv') 
+output_dir.mkdir(exist_ok=True)
+output_path = output_dir / 'snowmed_codes_2025.csv'
+
+#convert to csv
+df.write_csv(output_path)
